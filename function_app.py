2568abffdf65851ac587aa6a2d8cdf645f0325fc
tech dor <tech.aharoni@gmail.com>
2024-06-11 12:09:59 +0300

    seconf commit

--- function_app.py
+++ function_app.py
@@ -6,5 +6,5 @@ app = func.FunctionApp()
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="batch-pages-splitter",
                                connection="medicalanalysis_SERVICEBUS") 
 def BatchPagesSplitter(azservicebus: func.ServiceBusMessage):
-    logging.info('Python ServiceBus Queue trigger processed a message: %s',
+    logging.info('Python ServiceBusv Queue trigger processed a message: %s',
                 azservicebus.get_body().decode('utf-8'))
